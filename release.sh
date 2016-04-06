9c2e95b00e537b677c7ea7579aa358f3cc24b753
Alexander Trefz <alexander.trefz@gmail.com>
2016-04-06 12:56:19 +0200

    Reorder release.sh

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 node_modules/.bin/rollup src/hook.ts -o dist/hook.amd.js -c -f amd
 node_modules/.bin/rollup src/hook.ts -o dist/hook.cjs.js -c -f cjs
-node_modules/.bin/rollup src/hook.ts -o dist/hook.es6.js -c rollup.es6.config.js -f es6
 node_modules/.bin/rollup src/hook.ts -o dist/hook.umd.js -c -f umd
 node_modules/.bin/rollup src/hook.ts -o dist/hook.iife.js -c -f iife
+node_modules/.bin/rollup src/hook.ts -o dist/hook.es6.js -c rollup.es6.config.js -f es6
